7d315f67e106584c2c4d6f79bc040e1b3a5acc9a
Chintan Savalia <chintan2011@gmail.com>
2019-11-25 19:40:16 -0500

    added POST and DELETE to server.js

--- server/server.js
+++ server/server.js
@@ -7,6 +7,22 @@ app.get('/', function (req, res){
 	res.send("Get Request");
 });
 
+app.put('/', function (req, res){
+	console.log("HTTP PUT Request");
+	res.send("HTTP PUT Request");
+});
+
+app.post('/', function (req, res){
+	console.log("HTTP POST Request");
+	res.send("HTTP POST Request");
+});
+
+app.delete('/', function (req, res){
+	console.log("HTTP DELETE Request");
+	res.send("HTTP DELETE Request");
+});
+
+
 // start server on port 8080
 var server = app.listen(8080, function(){
 	var host = server.address().address;
