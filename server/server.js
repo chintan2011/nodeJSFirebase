f678fb034761ff11a0a13941af421dedb7f55447
Chintan Savalia <chintan2011@gmail.com>
2019-11-25 21:58:17 -0500

    fireBase config

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 var express = require('express'); //import express
+var firebase = require('firebase');
 var app = express();
 
 // handle HTTP Get requests
 app.get('/', function (req, res){
 	console.log("Get Request");
 	res.send("Get Request");
+	firebase.database().ref('/TestMessages').set({TestMessage: 'GET Request'});
 });
 
 app.put('/', function (req, res){
@@ -23,14 +25,16 @@ app.delete('/', function (req, res){
 });
 
 // initialize firebase
-var firebase = require('firebase');
+
 var config = {
-	apiKey: "APIKEY",
-	authDomain: "AUTHDOMAIN",
-	databaseURL: "TESTURL",
-	projectId: "PROJECTID",
-	storageBucket: "PROJECTBUCKET",
-	messagingSenderId: "ID"
+	apiKey: "AIzaSyDyz8b3ADJDc2nbrTZEZVWrcAIiOByCVG0",
+	authDomain: "nodejsfirebase-3feb2.firebaseapp.com",
+	databaseURL: "https://nodejsfirebase-3feb2.firebaseio.com",
+	projectId: "nodejsfirebase-3feb2",
+	storageBucket: "nodejsfirebase-3feb2.appspot.com",
+	messagingSenderId: "1003988804280",
+	appId: "1:1003988804280:web:d592ce92c13f91aadaca4d",
+	measurementId: "G-SDN84DT2SZ"
 };
 
 firebase.initializeApp(config);
